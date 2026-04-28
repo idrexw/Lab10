51af721c76bcc632507d04e961ad816405cb74ec
Andrew Maher <andrewmaher845@gmail.com>
2026-04-28 15:42:26 -0400

    UserService + AppointUserService

--- src/main/java/edu/secourse/salonapp/services/UserService.java
+++ src/main/java/edu/secourse/salonapp/services/UserService.java
@@ -1,4 +1,4 @@
 package edu.secourse.salonapp.services;
 
-public class Service {
+public class UserService {
 }
--- src/test/java/edu/secourse/salonapp/services/AppointmentUserServiceTest.java
+++ src/test/java/edu/secourse/salonapp/services/AppointmentUserServiceTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppointmentServiceTest {
+class AppointmentUserServiceTest {
 
     @Test
     void createAppointment() {
