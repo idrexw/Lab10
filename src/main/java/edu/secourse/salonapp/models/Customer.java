921e783ee46d738233004938c03c5c6e2e16b215
Andrew Maher <andrewmaher845@gmail.com>
2026-04-30 16:21:27 -0400

    Updated roles and removeUser, getUser, idk what else i did alot okay i forgot

--- src/main/java/edu/secourse/salonapp/models/Customer.java
+++ src/main/java/edu/secourse/salonapp/models/Customer.java
@@ -3,6 +3,7 @@ package edu.secourse.salonapp.models;
 public class Customer extends User{
 
     public Customer(String un, String pw, String name, String emAddr, String role) {
-        super(un, pw, name, emAddr, role);
+        super(un, pw, name, emAddr);
+        this.setRole(role);
     }
 }
--- src/main/java/edu/secourse/salonapp/services/UserService.java
+++ src/main/java/edu/secourse/salonapp/services/UserService.java
@@ -1,17 +1,45 @@
 package edu.secourse.salonapp.services;
 
+import edu.secourse.salonapp.models.Admin;
+import edu.secourse.salonapp.models.Customer;
+import edu.secourse.salonapp.models.Stylist;
 import edu.secourse.salonapp.models.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
-    private List<User> users;
-    public static void assignRole(User u, String s){
-        switch (s.toUpperCase()) {
-            case "S" -> u.setRole("Stylist");
-            case "A" -> u.setRole("Admin");
-            case "C" -> u.setRole("Customer");
-            default -> System.err.println("Role Not Assigned Properly to user " + u.getAccountId() + ".");
+    private final List<User> users;
+
+    public UserService(List<User> users) {
+        this.users = users;
+    }
+
+    public void createUser(String username, String password, String name, String email, String role){
+        User newUser;
+        switch (role.toUpperCase()){
+            case "S" -> { newUser = new Stylist(username, password, name, email, role); }
+            case "A" -> { newUser = new Admin(username, password, name, email, role); }
+            case "C" -> { newUser = new Customer(username, password, name, email, role); }
+            default -> {
+                System.err.println("Role Not Assigned Properly to user " + role.toUpperCase() + ".");
+                newUser = new User(username, password, name, email);
+            }
         }
+        this.users.add(newUser);
+    }
+
+    public void removeUser(int accountId) {
+        User user = getUser(accountId);
+        this.users.remove(user);
+    }
+
+    public User getUser(int accountId) {
+        for (User user : users) {
+            if (user.getAccountId() == accountId) {
+                return user;
+            }
+        }
+        return null;
     }
 }
