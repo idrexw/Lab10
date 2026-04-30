e41650d539189bfad158bdccf269413713786662
luifuata <lfuat1@brockport.edu>
2026-04-30 16:10:40 -0400

    updated customer class

--- src/main/java/edu/secourse/salonapp/models/Customer.java
+++ src/main/java/edu/secourse/salonapp/models/Customer.java
@@ -1,8 +1,9 @@
 package edu.secourse.salonapp.models;
 
-public class Customer extends User{
+public class Customer extends User {
 
-    public Customer(String un, String pw, String name, String emAddr, String role) {
-        super(un, pw, name, emAddr, role);
+    public Customer(String un, String pw, String name, String emAddr) {
+        super(un, pw, name, emAddr);
+        this.setRole("customer");
     }
 }
