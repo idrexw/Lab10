a15d8e4de63112d1814c179ed1d8cae8ef8e8d9d
Andrew Maher <andrewmaher845@gmail.com>
2026-04-30 16:23:50 -0400

    updated update

--- src/main/java/edu/secourse/salonapp/services/UserService.java
+++ src/main/java/edu/secourse/salonapp/services/UserService.java
@@ -5,7 +5,6 @@ import edu.secourse.salonapp.models.Customer;
 import edu.secourse.salonapp.models.Stylist;
 import edu.secourse.salonapp.models.User;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
@@ -18,9 +17,9 @@ public class UserService {
     public void createUser(String username, String password, String name, String email, String role){
         User newUser;
         switch (role.toUpperCase()){
-            case "S" -> { newUser = new Stylist(username, password, name, email, role); }
-            case "A" -> { newUser = new Admin(username, password, name, email, role); }
-            case "C" -> { newUser = new Customer(username, password, name, email, role); }
+            case "S" -> newUser = new Stylist(username, password, name, email, role);
+            case "A" -> newUser = new Admin(username, password, name, email, role);
+            case "C" -> newUser = new Customer(username, password, name, email, role);
             default -> {
                 System.err.println("Role Not Assigned Properly to user " + role.toUpperCase() + ".");
                 newUser = new User(username, password, name, email);
@@ -41,5 +40,9 @@ public class UserService {
             }
         }
         return null;
+    }
+
+    public List<User> getUsers() {
+        return users;
     }
 }
