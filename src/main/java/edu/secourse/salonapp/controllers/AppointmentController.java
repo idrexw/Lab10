257341516163578a375de74fd9667221470e8d69
cedar <cwill30@brockport.edu>
2026-04-30 16:27:36 -0400

    Done

--- src/main/java/edu/secourse/salonapp/controllers/AppointmentController.java
+++ src/main/java/edu/secourse/salonapp/controllers/AppointmentController.java
@@ -1,4 +1,46 @@
 package edu.secourse.salonapp.controllers;
 
+import edu.secourse.salonapp.models.Appointment;
+import edu.secourse.salonapp.services.AppointmentService;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class AppointmentController {
+
+    private AppointmentService appointmentService;
+    private Scanner scanner;
+
+    public AppointmentController(AppointmentService appointmentService){
+        this.appointmentService = appointmentService;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void createAppointmentInfo(){
+        System.out.println("Customer Id: ");
+        int customerId = Integer.parseInt(scanner.nextLine());
+        System.out.println("Stylist Id: ");
+        int stylistId = Integer.parseInt(scanner.nextLine());
+        System.out.println("Time (MM-dd-yyyy HH:mm): ");
+        String date = scanner.nextLine();
+        LocalDateTime time = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm"));
+
+        Appointment appt = new Appointment(customerId, stylistId, time);
+        appointmentService.createAppointment(appt);
+    }
+
+    public void cancelAppointmentInfo(){
+        System.out.println("Enter Appointment Id to remove: ");
+        int id = Integer.parseInt(scanner.nextLine());
+        appointmentService.cancelAppointment(id);
+    }
+
+    public void viewUserAppointments(){
+        System.out.println("Enter User Id: ");
+        int id = Integer.parseInt(scanner.nextLine());
+        for(Appointment a : appointmentService.getAppointmentsByUser(id)){
+            System.out.println("Id: " + a.getAppointmentId() + " | Status: " + a.getStatus());
+        }
+    }
+
 }
--- src/main/java/edu/secourse/salonapp/controllers/UserController.java
+++ src/main/java/edu/secourse/salonapp/controllers/UserController.java
@@ -1,4 +1,51 @@
 package edu.secourse.salonapp.controllers;
 
+
+import edu.secourse.salonapp.models.User;
+import edu.secourse.salonapp.services.UserService;
+import java.util.Scanner;
+
 public class UserController {
+    private UserService userService;
+    private Scanner scanner;
+
+        public UserController(UserService userService){
+            this.userService = userService;
+            this.scanner = new Scanner(System.in);
+
+        }
+
+        public void createUserInfo(){
+            System.out.print("Username: ");
+            String un = scanner.nextLine();
+            System.out.print("Password: ");
+            String pw = scanner.nextLine();
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            System.out.print("Email: ");
+            String email = scanner.nextLine();
+            System.out.print("Role (C/S/A): ");
+            String role = scanner.nextLine();
+
+            User newUser = new User(un, pw, name, email, role);
+            userService.createUser(newUser);
+
+        }
+
+        public void updateUserInfo(){
+            System.out.println("enter Account Id to update: ");
+            int id = Integer.parseInt(scanner.nextLine());
+            System.out.println("New name: ");
+            String name = scanner.nextLine();
+            System.out.println("New email: ");
+            String email = scanner.nextLine();
+            userService.updateUser(id, name, email);
+        }
+
+        public void removeUserInfo(){
+            System.out.println("Enter Account Id to remove: ");
+            int id = Integer.parseInt(scanner.nextLine());
+            userService.removeUser(id);
+        }
+
 }
